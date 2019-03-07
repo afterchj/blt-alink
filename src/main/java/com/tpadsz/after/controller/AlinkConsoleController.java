ab95de4116c80fdbf7604171859407024868f9ac
hongjian.chen <766256898@qq.com>
2019-03-07 11:32:30 +0800

    修改test目录结构

---
+++ src/main/java/com/tpadsz/after/controller/AlinkConsoleController.java
@@ -0,0 +1,15 @@
+package com.tpadsz.after.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by hongjian.chen on 2019/3/7.
+ */
+
+@Controller
+@RequestMapping("/lightConsole")
+public class AlinkConsoleController {
+
+
+}
---
+++ src/main/java/com/tpadsz/after/service/ConsoleLogService.java
@@ -0,0 +1,10 @@
+package com.tpadsz.after.service;
+
+import java.util.Map;
+
+/**
+ * Created by hongjian.chen on 2019/3/7.
+ */
+public interface ConsoleLogService {
+    void saveLog(Map map);
+}
