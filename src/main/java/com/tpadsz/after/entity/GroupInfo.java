0dee60d3fc8c7ca573dce6953d2348d93616958c
chenhao.lu <chenhao.lu>
2019-03-21 17:28:16 +0800

    修改提交老项目接口

---
+++ src/main/java/com/tpadsz/after/entity/GroupInfo.java
@@ -0,0 +1,52 @@
+package com.tpadsz.after.entity;
+
+/**
+ * Created by chenhao.lu on 2019/3/21.
+ */
+public class GroupInfo {
+    private String mname;
+    private Integer mid;
+    private Integer gid;
+    private Integer groupId;
+    private String gname;
+
+    public Integer getGid() {
+        return gid;
+    }
+
+    public void setGid(Integer gid) {
+        this.gid = gid;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGname() {
+        return gname;
+    }
+
+    public void setGname(String gname) {
+        this.gname = gname;
+    }
+}
---
+++ src/main/java/com/tpadsz/after/entity/LightInfo.java
@@ -0,0 +1,62 @@
+package com.tpadsz.after.entity;
+
+/**
+ * Created by chenhao.lu on 2019/3/21.
+ */
+public class LightInfo {
+
+    private String mname;
+    private Integer mid;
+    private String lmac;
+    private Integer groupId;
+    private String lname;
+    private String productId;
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+
+    public String getLmac() {
+        return lmac;
+    }
+
+    public void setLmac(String lmac) {
+        this.lmac = lmac;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
